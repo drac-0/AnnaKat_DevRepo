b38542cbbbf53c4444fa9fa3e9215940c9d3d9fe
drac-0 <muhammadfaiz050607@gmail.com>
2026-09-20 06:59:06 +0800

    testing for now

--- v0.1.1/new.c
+++ v0.1.1/new.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 /*
 # if a file hasn't modified since the last scan, then it's a waste of time to check it
@@ -12,6 +13,20 @@ That's it fella. That's the whole thing in this patch.
 */
 
 //do i really need to box a function for this?. We'll see about that later
+//
+//
+
+
+// this is for today, let's see if i can push more 
+//
+// What happen (Etherane):
+// 1. Struct tm checkpoint write
+// 2. Struct tm checkpoint read
+//
+// What's need to be changed:
+// 1. time_t instead of struct tm stored
+
+
 struct tm * tmNow(){
       struct tm * now;
       time_t t = time(NULL);
@@ -29,30 +44,28 @@ struct time {
       int year;
 };
 
+void Checkpoint(){
+      struct tm * point;
+      time_t t = time(NULL);
+
+      point = localtime(&t);
+      int Fp = open("log.bin", O_WRONLY | O_APPEND);
+      write(Fp, point, sizeof(int) * 9);
+      close(Fp);
+}
+
+struct tm * readCheckpoint(){
+      struct tm * pointfile = malloc(sizeof(int) * 9);
+      int Fp = open("log.bin", O_RDONLY);
+      read(Fp,pointfile,sizeof(int) * 9);
+      return pointfile;
+}
 
 int main(){
-      struct stat file;
-      stat("./Readme.md", &file);
-      struct tm * now;
-      struct tm * fltm;
+      Checkpoint();
 
+      struct tm * readpoint;
+      readpoint = readCheckpoint();
 
-      fltm = localtime(&file.st_mtim.tv_sec);
-      printf("%s", ctime(&file.st_mtim.tv_sec));
-
-      //printf("%d\n", ctime(file.st_mtim));
-
-      /*
-      time_t t = time(NULL);
-      now = localtime(&t);
-
-
-      // strip down the struct. I'll do it later
-
-      int len ;
-      int Fp = open("log.bin", O_WRONLY | O_APPEND);
-      write(Fp, &len, strlen(wtest));
-
-      close(Fp);
-      */
+      printf("%s", asctime(readpoint));
 }
